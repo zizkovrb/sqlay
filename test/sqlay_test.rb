3b4ed43369cf77c6743c34a4ce1c5dc6be32ea9d
Josef Šimánek <retro@ballgag.cz>
2013-06-29 14:41:24 +0200

    Move tests to memory.

--- test/sqlay_test.rb
+++ test/sqlay_test.rb
@@ -6,18 +6,16 @@ class TestSqlay < MiniTest::Unit::TestCase
   end
 
   def test_query_method_with_good_path
-    File.delete("test.db") if File.exists?("test.db")
-    assert Sqlay.execute("test.db", "SELECT date('now');")
+    assert Sqlay.execute(":memory:", "SELECT date('now');")
   end
 
   def test_query_method_with_bad_path
-    File.delete("test.db") if File.exists?("test.db")
-    refute Sqlay.execute("/a/c/d/e/test.db", "SELECT date('now');")
+    refute Sqlay.execute("/a/b/c/d", "SELECT date('now');")
   end
 
   def test_query_date
     date = Time.now.to_s.split(' ').first
-    query = Sqlay.execute("test.db", "SELECT date('now') AS date;")
+    query = Sqlay.execute(":memory:", "SELECT date('now') AS date;")
     assert_equal(date, query[0]["date"])
   end
 end
